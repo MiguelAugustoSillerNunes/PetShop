04f3fdc3c5a9db57bf6684b180542b3753815d89
João Vitor <joaobrambati@gmail.com>
2025-02-23 15:41:47 -0300

    wip

---
+++ static/js/tela-inicial.js
@@ -0,0 +1,61 @@
+const agendamentos = [
+    {
+        id: 1,
+        nome: 'Rex',
+        especie: 'Cachorro',
+        raca: 'Pastor Alemão',
+        servico: 'Banho',
+        data: '15/08/2025',
+    },
+    {
+        id: 2,
+        nome: 'Bobby',
+        especie: 'Cachorro',
+        raca: 'Labrador',
+        servico: 'Tosa',
+        data: '16/08/2025',
+    },
+    {
+        id: 3,
+        nome: 'Fido',
+        especie: 'Cachorro',
+        raca: 'Bulldog',
+        servico: 'Consulta Veterinária',
+        data: '17/08/2025',
+    }
+];
+
+// Função para preencher a tabela com os dados
+function preencherTabela() {
+    const tableBody = document.getElementById('tabela-dados');
+    
+    // Usando for para adicionar as linhas à tabela
+    for (let i = 0; i < agendamentos.length; i++) {
+        const agendamento = agendamentos[i];
+        const row = document.createElement('tr');
+
+        // Criar as células da linha
+        row.innerHTML = `
+            <th scope="row">${agendamento.id}</th>
+            <td>${agendamento.nome}</td>
+            <td>${agendamento.especie}</td>
+            <td>${agendamento.raca}</td>
+            <td>${agendamento.servico}</td>
+            <td>${agendamento.data}</td>
+            <td>
+                <button class="btn-visualizar" data-toggle="tooltip" title="Excluir" style="margin-right: 10px;">
+                    <i class="fas fa-trash"></i>
+                </button>
+                <button class="btn-visualizar" data-toggle="tooltip" title="Visualizar">
+                    <i class="fas fa-eye"></i> 
+                </button>
+            </td>
+        `;
+
+        // Adicionar a linha ao corpo da tabela
+        tableBody.appendChild(row);
+    }
+}
+
+// Chamar a função para preencher a tabela ao carregar a página
+window.onload = preencherTabela;
